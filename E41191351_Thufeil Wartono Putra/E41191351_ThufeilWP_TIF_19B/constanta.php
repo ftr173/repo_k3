34847303eb2ef1639c9b9d86f42d688ece6b79aa
thufeil wartono putra <71993022+Thufeil-E41191351@users.noreply.github.com>
2020-10-30 18:11:01 +0700

    Add files via upload

---
+++ E41191351_Thufeil Wartono Putra/E41191351_ThufeilWP_TIF_19B/constanta.php
@@ -0,0 +1,13 @@
+<?php // file : belajar-konstanta.php
+
+// membuat konstanta
+define('VERSION','1.0.0');
+
+const SITE_NAME = "Petanikode";
+const BASE_URL = "https://www.petanikode.com";
+
+//cetak konstanta
+echo "Site Name =" . SITE_NAME . "</br>";
+echo "URL =" . BASE_URL . "</br>";
+echo "Version =" . VERSION . "</br>";
+?>
