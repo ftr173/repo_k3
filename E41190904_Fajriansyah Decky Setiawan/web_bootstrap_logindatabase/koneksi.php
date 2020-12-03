902041989d87108ddb3eb7c6f0302c1342f8613c
deckystwn <71975374+deckystwn@users.noreply.github.com>
2020-12-03 22:35:28 +0700

    Summary

---
+++ E41190904_Fajriansyah Decky Setiawan/web_bootstrap_logindatabase/koneksi.php
@@ -0,0 +1,9 @@
+<?php
+
+    $koneksi = mysqli_connect("localhost", "root", "", "web_bootstrap");
+
+    if (mysqli_connect_error()) {
+        echo "Koneksi database gagal:".mysqli_connect_error();
+    }
+
+?>
